8a13193798f445ce9eeede4367bcdea5b54ec4b5
João <joaooliveirarech@hotmail.com>
2022-06-05 01:06:11 -0400

    chore: Adding PersonService

---
+++ src/app/service/PersonService.js
@@ -0,0 +1,30 @@
+const PersonRepository = require('../repository/PersonRepository')
+
+class PersonService {
+	static async create(payload) {
+		const result = await PersonRepository.createPerson(payload)
+		return result
+	}
+
+	static async listAll(payload) {
+		const result = await PersonRepository.listPerson(payload)
+		return result
+	}
+
+	static async getById(payload) {
+		const result = await PersonRepository.getPerson(payload)
+		return result
+	}
+
+	static async update(id, payload) {
+		const result = await PersonRepository.updatePerson(payload, id)
+		return result
+	}
+
+	static async delete(payload) {
+		const result = await PersonRepository.deletePerson(payload) 
+        return result
+	}
+}
+
+module.exports = new PersonService()
