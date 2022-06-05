c75c68f0a99fb184aadee76069a3d8f8885d6094
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-05 02:38:50 -0400

    fix: Wrong message for start the server

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const Port = 3000;
 
 app.listen(Port, () => {
-	console.log(`The serves is running on port ${Port}`);
+	console.log(`The server is running on port ${Port}`);
 });
