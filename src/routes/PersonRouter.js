dff9409574fded63f8a1862560dc8ad607dac625
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-05 11:43:58 -0400

    fix: Change the routes

---
+++ src/routes/PersonRouter.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const PersonController = require("../app/controller/PersonController");
+
+const router = express.Router();
+
+router
+	.post("/api/v1/person", PersonController.createPerson)
+	.get("/api/v1/person", PersonController.listAllPerson)
+	.get('/:id', PersonController.listById)
+	.put('/:id', PersonController.updatePerson)
+	.delete('/:id', PersonController.deletePerson)
+
+module.exports = router;
