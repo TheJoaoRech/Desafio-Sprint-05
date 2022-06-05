e7d7e85d843e3aaf96d0e2eede192ba8765d5ba1
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-05 12:50:52 -0400

    chore: Adding CarRouter to the app.use

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const {Router} = require('express')
 const PersonRouter = require('./PersonRouter')
+const CarRouter = require('./CarRouter')
 
 const routes = (app) => {
 	app.use(PersonRouter)
+	app.use(CarRouter)
 }
 module.exports=routes
