969abe26ba7c887e76d2fe8133d6dc307b5be8a7
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-06 00:32:22 -0400

    feat: Adding route to updatePerson with verifications

--- src/routes/PersonRouter.js
+++ src/routes/PersonRouter.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const PersonController = require("../app/controller/PersonController")
 const validatePerson = require("../app/validations/Person/validatePerson")
+const updatePerson = require("../app/validations/Person/updatePerson")
 
 const router = express.Router()
 
@@ -8,7 +9,7 @@ router
 	.post("/api/v1/person", validatePerson, PersonController.create)
 	.get("/api/v1/person", PersonController.list)
 	.get("/api/v1/person/:id", PersonController.getById)
-	.put("/api/v1/person/:id", PersonController.update)
+	.put("/api/v1/person/:id", updatePerson, PersonController.update)
 	.delete("/api/v1/person/:id", PersonController.delete)
 
 module.exports = router
