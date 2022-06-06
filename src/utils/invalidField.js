dd04f1cb5926151b5159ffff4ba5d8878c51abc1
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-06 02:52:59 -0400

    feat: invalidField to work with authValidation

---
+++ src/utils/invalidField.js
@@ -0,0 +1,13 @@
+class InvalidField extends Error {
+	constructor(field) {
+		super()
+		this.name = "InvalidField"
+		this.status = 400
+		this.message = [
+			{
+				message: this.name, 
+				details: [{	message: `The field: ${field} it's wrong!`}]}]
+	}
+}
+
+module.exports = InvalidField
