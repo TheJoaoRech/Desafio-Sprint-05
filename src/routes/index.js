3101a013ced0a1c77aef9e687b00c2c8dbab23ad
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-06 00:57:03 -0400

    feat: Adding AuthenticateRoute

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const {Router} = require('express')
 const PersonRouter = require('./PersonRouter')
 const CarRouter = require('./CarRouter')
+const AuthenticateRoute = require('./AuthRouter')
 
 const routes = (app) => {
 	app.use(PersonRouter)
 	app.use(CarRouter)
+	app.use(AuthenticateRoute)
 }
 module.exports=routes
