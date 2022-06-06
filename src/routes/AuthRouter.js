4f20f6e0cd6c8d6fa67a3ba8a7d2f61d18751715
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-06 02:57:49 -0400

    Creating a AuthRoute

--- src/routes/AuthRouter.js
+++ src/routes/AuthRouter.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const AuthController = require('../app/controller/AuthController')
+const AuthValidate = require('../utils/authValidation')
 
 const router = express.Router()
 
 router
-	.post("/api/v1/authenticate", AuthController.auth)
+	.post("/api/v1/authenticate", AuthValidate, AuthController.auth)
 
 module.exports = router
