3784e26ef484f58547f4b397218814a0f19c21f2
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-06 00:21:14 -0400

    Adding validate to the personroutes

--- src/routes/PersonRouter.js
+++ src/routes/PersonRouter.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const PersonController = require("../app/controller/PersonController")
+const validatePerson = require("../app/validations/Person/validatePerson")
 
 const router = express.Router()
 
 router
-	.post("/api/v1/person", PersonController.create)
+	.post("/api/v1/person", validatePerson, PersonController.create)
 	.get("/api/v1/person", PersonController.list)
 	.get("/api/v1/person/:id", PersonController.getById)
 	.put("/api/v1/person/:id", PersonController.update)
