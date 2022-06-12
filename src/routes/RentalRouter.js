07cde6c015a3d71e327bb51142b6c9f95b6feea4
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-12 17:40:23 -0400

    feat: Adding verifications to the create route

--- src/routes/RentalRouter.js
+++ src/routes/RentalRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const RentalController = require('../app/controller/RentalController');
+const ValidateRental = require('../app/validations/Rental/validateRental');
 
 const router = express.Router();
 
 router
-	.post('/api/v1/rental', RentalController.create)
+	.post('/api/v1/rental', ValidateRental, RentalController.create)
 	.get('/api/v1/rental', RentalController.list)
 	.get('/api/v1/rental/:id', RentalController.getById)
 	.put('/api/v1/rental/:id', RentalController.update)
