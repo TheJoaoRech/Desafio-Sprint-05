6b702e80a0391f4be53b78467dbe5f67498aba4f
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-13 11:16:33 -0400

    feat: Creating a Swagger route to document the API

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const PersonRouter = require('./PersonRouter');
 const CarRouter = require('./CarRouter');
 const AuthenticateRouter = require('./AuthRouter');
 const RentalRouter = require('./RentalRouter');
+const SwaggerRouter = require('./SwaggerRouter');
 
 const routes = (app) => {
 	app.use(PersonRouter);
 	app.use(CarRouter);
 	app.use(AuthenticateRouter);
 	app.use(RentalRouter);
+	app.use(SwaggerRouter);
 };
 module.exports = routes;
