d6df0b525b79110a6e59a500afd74db2e30e9a4e
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-12 20:31:10 -0400

    feat: Creating a updateRental requirements

--- src/routes/RentalRouter.js
+++ src/routes/RentalRouter.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const RentalController = require('../app/controller/RentalController');
-const ValidateRental = require('../app/validations/Rental/validateRental');
+const validateRental = require('../app/validations/Rental/validateRental');
+const updateRental = require('../app/validations/Rental/validateRental');
 
 const router = express.Router();
 
 router
-	.post('/api/v1/rental', ValidateRental, RentalController.create)
+	.post('/api/v1/rental', validateRental, RentalController.create)
 	.get('/api/v1/rental', RentalController.list)
 	.get('/api/v1/rental/:id', RentalController.getById)
-	.put('/api/v1/rental/:id', RentalController.update)
+	.put('/api/v1/rental/:id', updateRental, RentalController.update)
 	.delete('/api/v1/rental/:id', RentalController.delete);
 
 module.exports = router;
