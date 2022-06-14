e14248b71ca98cd23b231ba9a5b5150644db23df
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-13 23:41:43 -0400

    fix: fixing some bugs on the rental validation

--- src/app/validations/Rental/updateRental.js
+++ src/app/validations/Rental/updateRental.js
@@ -1,15 +1,15 @@
-const Joi = require('joi');
+const Joi = require('Joi');
 
 module.exports = async (req, res, next) => {
 	try {
 		const schemaRental = Joi.object({
 			
 			name: Joi.string().min(2),
-			cnpj: Joi.string().regex(/^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/).message('Your CNPJ should only contain characters accepted by the system!'),
+			cnpj: Joi.string().regex(/^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/),
 			activities: Joi.string().min(5),
 			address: Joi.array().min(1).items(
 				{
-					zipCode: Joi.string().regex(/^\d{5}-\d{3}$/).message('Your zipCode should only contain characters accepted by the system!'),
+					zipCode: Joi.string().regex(/^\d{5}-\d{3}$/),
 					street: Joi.string().min(2),
 					complement: Joi.string(),
 					number: Joi.number().min(1),
@@ -26,7 +26,7 @@ module.exports = async (req, res, next) => {
 	} catch (error) {
 		return res.status(400).json(
 			error.details.map((detail) => ({
-				name: detail.path.join('.'),
+				name: detail.path.join(),
 				description: detail.message
 			})));
 	}
--- src/app/validations/Rental/validateRental.js
+++ src/app/validations/Rental/validateRental.js
@@ -5,11 +5,11 @@ module.exports = async (req, res, next) => {
 		const schemaRental = Joi.object({
 
 			name: Joi.string().min(2).required(),
-			cnpj: Joi.string().regex(/^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/).required().message('Your CNPJ should only contain characters accepted by the system!'),
+			cnpj: Joi.string().regex(/^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/).required(),
 			activities: Joi.string().min(5).required(),
-			address: Joi.array().min(1).items( 
+			address: Joi.array().min(1).items(
 				{
-					zipCode: Joi.string().regex(/^\d{5}-\d{3}$/).required().message('Your zipCode should only contain characters accepted by the system!'),
+					zipCode: Joi.string().regex(/^\d{5}-\d{3}$/).required(),
 					street: Joi.string().min(2),
 					complement: Joi.string(),
 					number: Joi.number().min(1).required(),
@@ -26,7 +26,7 @@ module.exports = async (req, res, next) => {
 	} catch (error) {
 		return res.status(400).json(
 			error.details.map((detail) => ({
-				name: detail.path.join('.'),
+				name: detail.path.join(),
 				description: detail.message
 			})));
 	}
