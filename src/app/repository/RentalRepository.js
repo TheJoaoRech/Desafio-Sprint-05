fa3d2a1fc0153110115c874876ac96dc5365f367
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-13 21:10:54 -0400

    fix: Fixing the rental paginate on the route GET

--- src/app/repository/RentalRepository.js
+++ src/app/repository/RentalRepository.js
@@ -14,24 +14,15 @@ class RentalRepository {
 	}
 
 	static async list(payload) {
-		const paginate = {
-			totalDocs: 'total',
-			docs: 'people',
-			page: 'offset',
-			totalPages: 'offsets',
-			nextPage: false,
-			prevPage: false,
-			pagingCounter: false,
-			meta: false,
-			hasPrevPage: false,
-			hasNextPage: false
+		const costumizePaginate = {totalDocs: 'total', docs: 'Rentals', page: 'offset', nextPage: false, prevPage: false, totalPages: 'offsets', pagingCounter: false, meta: false, hasPrevPage: false, hasNextPage: false
 		};
+		const {limit = 100, offset = 0, ...query} = payload;  
 		const options = {
-			limit: 5,
-			offset: 1,
-			customLabels: paginate
+			limit: Number(limit),
+			offset: Number(offset),
+			customLabels: costumizePaginate
 		};
-		return await RentalSchema.paginate(payload, {}); //Options Bugado.
+		return await RentalSchema.paginate(query, options);
 	}
 
 	static async getById(payload) {
