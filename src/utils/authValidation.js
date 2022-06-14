f034b51b2df48126e8a34bbed2b98642a7a2936b
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-14 15:50:51 -0400

    fix: returning 204 code on the body of authentification

--- src/utils/authValidation.js
+++ src/utils/authValidation.js
@@ -1,9 +1,9 @@
-const joi = require('joi');
+const Joi = require('joi');
 const InvalidField = require('../utils/invalidField');
 
-const autPost = joi.object({
-	email: joi.string().email().required().error(new InvalidField('email')),
-	password: joi.string().min(6).required().error(new InvalidField('password')),
+const autPost = Joi.object({
+	email: Joi.string().email().required().error(new InvalidField('email')),
+	password: Joi.string().min(6).required().error(new InvalidField('password')),
 });
 
 module.exports = async (req, res, next) => {
@@ -14,7 +14,11 @@ module.exports = async (req, res, next) => {
 			next();
 		}
 
-	} catch(error) {
-		return res.status(400).json({Error: error.message});
+	} catch (error) {
+		return res.status(400).json(
+			error.details.map((detail) => ({
+				name: detail.path.join('.'),
+				description: detail.message
+			})));
 	}
 };
