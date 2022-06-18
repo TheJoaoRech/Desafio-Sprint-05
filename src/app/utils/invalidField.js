6baff2eaf7f52e75f9024f1b5739d18af7f28757
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-18 17:00:53 -0400

    fix: Fixing message of the error on empty field

--- src/app/utils/invalidField.js
+++ src/app/utils/invalidField.js
@@ -2,11 +2,7 @@ class InvalidField extends Error {
 	constructor(field) {
 		super();
 		this.name = 'InvalidField';
-		this.status = 400;
-		this.message = [
-			{
-				message: this.name, 
-				details: [{	message: `The field: ${field} it's wrong!`}]}];
+		this.description = `This field: "${field}" it's wrong!`;
 	}
 }
 
