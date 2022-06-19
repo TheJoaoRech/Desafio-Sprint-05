f6f867f0f449e36aa9b87b1070ad575548aa3396
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-19 17:14:32 -0400

    feat: Putting the validations on the fleet route

--- src/routes/FleetRouter.js
+++ src/routes/FleetRouter.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const FleetController = require('../app/controller/FleetController');
+const validateFleet = require('../app/validations/Fleet/validateFleet');
+const updateFleet = require('../app/validations/Fleet/updateFleet');
 
 const router = express.Router();
 
 router
-	.post('/api/v1/rental/:id_rental/fleet', FleetController.create)
+	.post('/api/v1/rental/:id_rental/fleet', validateFleet, FleetController.create)
 	.get('/api/v1/rental/:id_rental/fleet', FleetController.list)
 	.get('/api/v1/rental/:id_rental/fleet/:id', FleetController.getById)
-	.put('/api/v1/rental/:id_rental/fleet/:id', FleetController.update)
+	.put('/api/v1/rental/:id_rental/fleet/:id', updateFleet, FleetController.update)
 	.delete('/api/v1/rental/:id_rental/fleet/:id', FleetController.delete);
 
 module.exports = router;
