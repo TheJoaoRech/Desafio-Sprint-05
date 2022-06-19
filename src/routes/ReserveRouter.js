853b1f21ea3aefb992924b397dd6a062fc24fe75
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-19 18:21:01 -0400

    feat: Creating get and delete verifications on the route reserve

--- src/routes/ReserveRouter.js
+++ src/routes/ReserveRouter.js
@@ -2,14 +2,16 @@ const express = require('express');
 const ReserveController = require('../app/controller/ReserveController');
 const validateReserve = require('../app/validations/Reserve/validateReserve');
 const updateReserve = require('../app/validations/Reserve/updateReserve');
+const getReserve = require('../app/validations/Reserve/getReserve');
+const deleteReserve = require('../app/validations/Reserve/deleteReserve');
 
 const router = express.Router();
 
 router
 	.post('/api/v1/rental/:id_rental/reserve', validateReserve, ReserveController.create)
-	.get('/api/v1/rental/:id_rental/reserve', ReserveController.list)
-	.get('/api/v1/rental/:id_rental/reserve/:id', ReserveController.getById)
+	.get('/api/v1/rental/:id_rental/reserve', getReserve, ReserveController.list)
+	.get('/api/v1/rental/:id_rental/reserve/:id', getReserve, ReserveController.getById)
 	.put('/api/v1/rental/:id_rental/reserve/:id', updateReserve, ReserveController.update)
-	.delete('/api/v1/rental/:id_rental/reserve/:id', ReserveController.delete);
+	.delete('/api/v1/rental/:id_rental/reserve/:id', deleteReserve, ReserveController.delete);
 
 module.exports = router;
