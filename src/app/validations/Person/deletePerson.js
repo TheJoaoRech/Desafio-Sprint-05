3c86dba5fe7a699ffe45ddf90796be06c6e81499
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-19 18:12:32 -0400

    feat: Creating get and delete verifications on the route person

---
+++ src/app/validations/Person/deletePerson.js
@@ -0,0 +1,32 @@
+const Joi = require('joi').extend(require('@joi/date'));
+const birthDayValidate = require('../../../app/utils/birthDayValidate');
+//const cpfValidation = require('../../../app/utils/cpfValidation');
+
+module.exports = async (req, res, next) => {
+	try {
+
+		const schemaPerson = Joi.object({
+
+			name: Joi.string().min(3).max(30).trim(),
+			cpf: Joi.string().regex(/^[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}-?[0-9]{2}/).message('Your CPF should only contain characters accepted by the system!'),
+			birthDay: Joi.date().format('DD/MM/YYYY').max(birthDayValidate()),
+			email: Joi.string().min(10).email().lowercase().trim(),
+			password: Joi.string().min(6),
+			canDrive: Joi.string().valid('yes', 'no')
+		});
+
+		//if(!cpfValidation(req.body.cpf)) throw {message: 'Your CPF is invalid!'};
+		const { error } = await schemaPerson.validate(req.body, { abortEarly: false });
+		if (error) throw error;
+		return next();
+		
+	} catch (error) {
+		if(error.details === undefined) {
+			return res.status(400).json({Error: error.message});
+		}
+		return res.status(400).json(
+			error.details.map((detail) => ({
+				name: detail.path.join(),
+				description: detail.message
+			})));}
+};
--- src/routes/PersonRouter.js
+++ src/routes/PersonRouter.js
@@ -2,14 +2,15 @@ const express = require('express');
 const PersonController = require('../app/controller/PersonController');
 const validatePerson = require('../app/validations/Person/validatePerson');
 const updatePerson = require('../app/validations/Person/updatePerson');
-
+const getPerson = require('../app/validations/Person/getPerson');
+const deletePerson = require('../app/validations/Person/deletePerson');
 const router = express.Router();
 
 router
 	.post('/api/v1/person', validatePerson, PersonController.create)
-	.get('/api/v1/person', PersonController.list)
-	.get('/api/v1/person/:id', PersonController.getById)
+	.get('/api/v1/person', getPerson, PersonController.list)
+	.get('/api/v1/person/:id', getPerson, PersonController.getById)
 	.put('/api/v1/person/:id', updatePerson, PersonController.update)
-	.delete('/api/v1/person/:id', PersonController.delete);
+	.delete('/api/v1/person/:id', deletePerson, PersonController.delete);
 
 module.exports = router;
