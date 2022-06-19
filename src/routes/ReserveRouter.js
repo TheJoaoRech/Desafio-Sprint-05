34c1dda96a3d38cc72e45b0b3ac3eeb521cbe538
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-19 17:14:55 -0400

    feat: Putting the validations on the reserve route

--- src/routes/ReserveRouter.js
+++ src/routes/ReserveRouter.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const ReserveController = require('../app/controller/ReserveController');
+const validateReserve = require('../app/validations/Reserve/validateReserve');
+const updateReserve = require('../app/validations/Reserve/updateReserve');
 
 const router = express.Router();
 
 router
-	.post('/api/v1/rental/:id_rental/reserve', ReserveController.create)
+	.post('/api/v1/rental/:id_rental/reserve', validateReserve, ReserveController.create)
 	.get('/api/v1/rental/:id_rental/reserve', ReserveController.list)
 	.get('/api/v1/rental/:id_rental/reserve/:id', ReserveController.getById)
-	.put('/api/v1/rental/:id_rental/reserve/:id', ReserveController.update)
+	.put('/api/v1/rental/:id_rental/reserve/:id', updateReserve, ReserveController.update)
 	.delete('/api/v1/rental/:id_rental/reserve/:id', ReserveController.delete);
 
 module.exports = router;
