c850c0c89ed525a3d03ccc7491bfe663e8500216
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-19 18:17:50 -0400

    feat: Creating get and delete verifications on the route rental

--- src/routes/RentalRouter.js
+++ src/routes/RentalRouter.js
@@ -2,14 +2,16 @@ const express = require('express');
 const RentalController = require('../app/controller/RentalController');
 const validateRental = require('../app/validations/Rental/validateRental');
 const updateRental = require('../app/validations/Rental/updateRental');
+const getRental = require('../app/validations/Rental/updateRental');
+const deleteRental = require('../app/validations/Rental/updateRental');
 
 const router = express.Router();
 
 router
 	.post('/api/v1/rental', validateRental, RentalController.create)
-	.get('/api/v1/rental', RentalController.list)
-	.get('/api/v1/rental/:id', RentalController.getById)
+	.get('/api/v1/rental', getRental, RentalController.list)
+	.get('/api/v1/rental/:id', getRental, RentalController.getById)
 	.put('/api/v1/rental/:id', updateRental, RentalController.update)
-	.delete('/api/v1/rental/:id', RentalController.delete);
+	.delete('/api/v1/rental/:id', deleteRental, RentalController.delete);
 
 module.exports = router;
