30e1749a7bf7d70f9ed90c42163306e9b542bfcf
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-20 18:06:40 -0400

    chore: Moving some files and delete others

--- __tests__/carTest/createCar.test.js
+++ __tests__/carTest/createCar.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 const request = require('supertest');
-const app = require('../../../src/app');
+const app = require('../../src/app');
 
 describe('Create a car',() => {
 	it('Create a car for test!', async () => {
--- __tests__/personTest/createPerson.Test.js
+++ __tests__/personTest/createPerson.Test.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 const request = require('supertest');
-const app = require('../../../src/app');
+const app = require('../../src/app');
 
 describe('Create a person',() => {
 	it('Create a person for test!', async () => {
