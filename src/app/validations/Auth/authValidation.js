8e23e87e38fab8ac1301c4c021ec5f707093ec57
TheJoaoRech <joaooliveirarech@hotmail.com>
2022-06-26 19:41:41 -0400

    fix: moving the authenticate to verifications

--- src/app/validations/Auth/authValidation.js
+++ src/app/validations/Auth/authValidation.js
@@ -1,5 +1,5 @@
 const Joi = require('joi');
-const InvalidField = require('./invalidField');
+const InvalidField = require('../../utils/invalidField');
 
 const autPost = Joi.object({
   email: Joi.string().email().required().error(new InvalidField('email')),
--- src/routes/AuthRouter.js
+++ src/routes/AuthRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const AuthController = require('../app/controller/AuthController');
-const AuthValidate = require('../app/utils/authValidation');
+const AuthValidate = require('../app/validations/Auth/authValidation');
 
 const router = express.Router();
 
